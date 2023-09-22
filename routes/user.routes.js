c3a78888991d23e8e88fd2f9d51f83181c4aaf37
Sandrine Manel <sandrine.manelgmail.com>
2023-09-22 10:51:41 +0200

    fixed user get request to get user by email

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,8 +5,9 @@ const fileUploader = require("../config/cloudinary.config");
 const User = require("../models/User.model");
 
 // GET /api/user -  Retrieves a specific user 
-router.get("/user", async (req, res, next) =>  {
-    let result = await User.findOne(req.body);
+router.get("/user/:useremail", async (req, res, next) =>  {
+    console.log(req.params.useremail);
+    let result = await User.findOne({email: req.params.useremail});
     console.log(result);
     res.json(result)
 });
