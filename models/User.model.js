ca794f6113c6e80b35f9bb62905ad947cbef7564
Utku Cikmaz <utkucikmaz@gmail.com>
2023-09-25 16:04:51 +0200

    cleaning unnecessary comments on user route and user model

--- models/User.model.js
+++ models/User.model.js
@@ -1,6 +1,5 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
     {
         email: {
@@ -26,7 +25,6 @@ const userSchema = new Schema(
         },
     },
     {
-        // this second object adds extra properties: `createdAt` and `updatedAt`
         timestamps: true,
     }
 );
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -11,15 +11,10 @@ router.get("/user/:useremail", async (req, res, next) => {
 });
 // POST "/api/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
 router.post("/upload", fileUploader.single("imageUrl"), (req, res, next) => {
-    // console.log("file is: ", req.file)
-
     if (!req.file) {
         next(new Error("No file uploaded!"));
         return;
     }
-
-    // Get the URL of the uploaded file and send it as a response.
-    // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
 
     res.json({ fileUrl: req.file.path });
 });
